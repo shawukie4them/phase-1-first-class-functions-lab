b81e024a0dd1d578ffd40caeb44ad2eab6e284ee
shawukie4them <shawukiekawuki@gmail.com>
2024-07-01 15:27:00 +0300

    advanced functions

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+//Exercise 1
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0, 2);
+}
+
+//Exercise 2
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(-2);
+}
+
+//Exercise 3
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers,
+];
+
+//Exercise 4
+function createFareMultiplier(multiplier){
+    return function(value){
+        return value * multiplier;
+    };
+}
+
+//Exercise 5
+function fareDoubler(fare){
+    return fare *2;
+}
+
+//Exercise 6
+function fareTripler(fare){
+    return fare * 3;
+}
+
+//Exercise 7
+function selectDifferentDrivers(fardrivers, fn){
+    return fn(fardrivers);
+}
